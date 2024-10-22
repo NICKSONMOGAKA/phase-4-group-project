0d128f64f31b0954cb7add8ab9f9e78b71122454
Ian Mwangi <ian.mwangi2@student.moringaschool.com>
2024-10-22 10:05:20 +0300

    installed flask_cors

--- server/app.py
+++ server/app.py
@@ -2,9 +2,10 @@ from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
 from models import *
-
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 migrate=Migrate(app, db)
